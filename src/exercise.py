4bce97eb171249e582bba968534b61e514bcaa46
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-19 02:33:18 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    while True:
+        ans = input('Carry on?')
+        if ans == 'no':
+            break
+            
+    
+    
 
 if __name__ == '__main__':
     main()
